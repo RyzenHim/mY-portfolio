b2ed789de730882345c2cf9a1136ce25ee9e57d2
Himanshu Amulya Upadhyay <90633968+RyzenHim@users.noreply.github.com>
2024-08-25 04:28:10 +0000

    js added in ham

--- hamburger/hamburger.js
+++ hamburger/hamburger.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
-
+const hamMenu = document.querySelector(".collapsable_menu_main_div");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("change");
-  console.log("added");
+  hamMenu.classList.toggle("collapsable_menu_main_div_active");
+  checkMenuState();
 });
+
+function checkMenuState() {
+  if (hamburger.classList.contains("collapsable_menu_main_div_active")) {
+    console.log("collapsable_menu_main_div_active it contains ");
+  }
+}
